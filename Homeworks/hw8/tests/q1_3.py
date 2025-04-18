8642b863ad49913f6f1fb688f25f6fdef19b81bd
Andy <hoandy2296@gmail.com>
2025-04-18 14:08:56 -0700

    Add files via upload

---
+++ Homeworks/hw8/tests/q1_3.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_3',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> "value_category" in cal_housing.columns\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isin(cal_housing["value_category"].unique(), [0,1,2,3])\narray([ True,  True,  True,  True])','hidden': False,'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ Homeworks/hw8/tests/q3_1.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q3_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> cal_housing["inc_housing_ratio"].max() < 5\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> cal_housing["inc_housing_ratio"].min() > 0\nTrue','hidden': False,'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
